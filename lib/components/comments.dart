5303c32082bee45ea438842726f53b3549aa3dee
ettorto <ernest.torto@ashesi.edu.gh>
2024-07-28 03:44:57 +0000

    new changes

---
+++ lib/components/comments.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Comment extends StatelessWidget {
+  final String text;
+  final String user;
+  final String time;
+  const Comment({
+    super.key,
+    required this.text,
+    required this.time,
+    required this.user,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Column(
+        children: [
+          //comment
+          Text(text),
+
+          //user and time
+          Row(
+            children: [
+              Text(user),
+              Text("."),
+              Text(time),
+            ],
+          )
+
+          
+
+        ],),
+    );
+  }
+}
