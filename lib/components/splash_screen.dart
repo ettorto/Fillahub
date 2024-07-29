2f7d46c03c9ed2b1308a3111ecec889439cdbeba
ettorto <ernest.torto@ashesi.edu.gh>
2024-07-29 14:56:17 +0000

    new changes

---
+++ lib/components/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:fillahub/auth/auth.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToAuthPage();
+  }
+
+  _navigateToAuthPage() async {
+    await Future.delayed(Duration(seconds: 3), () {}); // Duration of the splash screen
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => AuthPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add your logo or splash screen content here
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            Text('Welcome to Fillahub', style: TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
